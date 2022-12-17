a2fedea253de4a0a2523dbcce99f81f93e51a877
chelseaerinavery <chelsea.erin.avery@gmail.com>
2022-12-16 19:12:22 -0700

    completed direwolf and stark tests

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,15 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  }
 
+  greet() {
+    return "Hi, " + this.rider + "!";
+  }
+
+  eat() {}
+}
+
+module.exports = Dragon;
