168852200bdf916862da8e520dece0263338b341
chelseaerinavery <chelsea.erin.avery@gmail.com>
2022-12-19 18:16:06 -0700

    completed dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -3,13 +3,19 @@ class Dragon {
     this.name = name;
     this.rider = rider;
     this.hungry = true;
+    this.meals = 0;
   }
 
   greet() {
     return "Hi, " + this.rider + "!";
   }
 
-  eat() {}
+  eat() {
+    this.meals += 1;
+    if (this.meals >= 3) {
+      this.hungry = false;
+    }
+  }
 }
 
 module.exports = Dragon;
