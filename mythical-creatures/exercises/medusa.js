cfbe4d22929e4718ebbceee9bc974b84645f5312
chelseaerinavery <chelsea.erin.avery@gmail.com>
2023-01-09 17:48:26 -0700

    completed medusa, person, statue, and ogre tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -6,6 +6,13 @@ class Medusa {
     this.name = name;
     this.statues = [];
   }
+
+  gazeAtVictim(victim) {
+    this.statues.push(new Statue(victim.name));
+    if (this.statues.length > 3) {
+      return new Person(this.statues.shift().name, "relieved");
+    }
+  }
 }
 
 module.exports = Medusa;
--- mythical-creatures/test/ogre-test.js
+++ mythical-creatures/test/ogre-test.js
@@ -1,39 +1,39 @@
-const assert = require('chai').assert;
-const Ogre = require('../exercises/ogre');
-const Human = require('../exercises/human');
+const assert = require("chai").assert;
+const Ogre = require("../exercises/ogre");
+const Human = require("../exercises/human");
 
-describe('Ogre', () => {
+describe("Ogre", () => {
   // Oh no! It looks like there's no text in the `it` blocks! I guess you'll have to read the tests SUPER carefully to know what the test is expecting!
 
-  it.skip('', function() {
-    const ogre = new Ogre({name:'Brak'});
-    assert.equal(ogre.name, 'Brak');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak" });
+    assert.equal(ogre.name, "Brak");
   });
 
-  it.skip('', function() {
-    const ogre = new Ogre({name: 'Brak'});
-    assert.equal(ogre.home, 'Swamp');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak" });
+    assert.equal(ogre.home, "Swamp");
   });
 
-  it.skip('', function() {
-    const ogre = new Ogre({name: 'Brak', abode: 'The Ritz'});
-    assert.equal(ogre.home, 'The Ritz');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak", abode: "The Ritz" });
+    assert.equal(ogre.home, "The Ritz");
   });
 
-  it.skip('', function() {
-    const ogre = new Ogre({name: 'Brak'});
-    const human = new Human('Jane');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak" });
+    const human = new Human("Jane");
 
-    assert.equal(human.name, 'Jane');
-    assert.equal(human.encounterCounter, 0)
+    assert.equal(human.name, "Jane");
+    assert.equal(human.encounterCounter, 0);
 
     ogre.encounter(human);
     assert.equal(human.encounterCounter, 1);
   });
 
-  it.skip('', function() {
-    const ogre = new Ogre({name: 'Brak'});
-    const human = new Human('Jane');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak" });
+    const human = new Human("Jane");
 
     ogre.encounter(human);
     ogre.encounter(human);
@@ -43,9 +43,9 @@ describe('Ogre', () => {
     assert.equal(human.noticesOgre(), true);
   });
 
-  it.skip('', function() {
-    const ogre = new Ogre({name: 'Brak'});
-    const human = new Human('Jane');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak" });
+    const human = new Human("Jane");
 
     assert.equal(human.noticesOgre(), false);
 
@@ -62,19 +62,19 @@ describe('Ogre', () => {
     assert.equal(human.noticesOgre(), true);
   });
 
-  it.skip('', function() {
-    const ogre = new Ogre({name: 'Brak'});
-    const human = new Human('Jane');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak" });
+    const human = new Human("Jane");
 
-    assert.equal(ogre.swings, 0)
+    assert.equal(ogre.swings, 0);
     ogre.swingAt(human);
 
     assert.equal(ogre.swings, 1);
   });
 
-  it.skip('', function() {
-    const ogre = new Ogre({name: 'Brak'});
-    const human = new Human('Jane');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak" });
+    const human = new Human("Jane");
 
     ogre.encounter(human);
     assert.equal(ogre.swings, 0);
@@ -86,11 +86,11 @@ describe('Ogre', () => {
     assert.equal(ogre.swings, 1);
   });
 
-  it.skip('', function() {
-    const ogre = new Ogre({name: 'Brak'});
-    const human = new Human('Jane');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak" });
+    const human = new Human("Jane");
 
-    assert.equal(human.knockedOut, false)
+    assert.equal(human.knockedOut, false);
     ogre.encounter(human);
     ogre.encounter(human);
     ogre.encounter(human);
@@ -103,9 +103,9 @@ describe('Ogre', () => {
     assert.equal(human.knockedOut, true);
   });
 
-  it.skip('', function() {
-    const ogre = new Ogre({name: 'Brak'});
-    const human = new Human('Jane');
+  it("", function () {
+    const ogre = new Ogre({ name: "Brak" });
+    const human = new Human("Jane");
 
     ogre.encounter(human);
     ogre.encounter(human);
